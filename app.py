33d7736b4a32bf6502a82dbe7244bb6f9e901245
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-06-02 10:54:40 +0700

    Flask render_templates

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello khematat"
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
-    return "<h1>About Me</h1>"
+    return render_template("about.html")
 
 @app.route('/admin')
 def profile():
-    return "<h1>Sawasdeekub Admin</h1>"
+    return render_template("admin.html")
 
-@app.route('/user/<name>/<age>')
-def member(name,age):
-    return "<h1>Hello member : {} , Age : {}</h1>".format(name,age)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
